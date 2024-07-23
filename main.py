b221ffe67ca6aa553b6bc3f9b7f7633fdc5e385e
Asher Zaczepinski <asherzac2020@gmail.com>
2024-07-22 23:30:30 -0700

    keepworking

--- main.py
+++ main.py
@@ -272,6 +272,11 @@ def process_line(input_y, img_array, width, difference_between_lines_for_line_dr
                                             keep_going = False
                                         else:
                                             keep_going = False
+
+
+
+                                        #implement the changed idifrection once!!!!
+                                                
                                         #first do the lines
                                         #put in the changed direction once thingy!!!
                                     #for the dashed white notes same logic for everything remember we changed up a lot of stuff so its gonna be a lot of work
